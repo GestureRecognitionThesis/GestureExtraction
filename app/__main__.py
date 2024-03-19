c24ac52f3508f76975843c870e8102c6aaa4251f
Nick / Tempus <56578594+NickDue@users.noreply.github.com>
2024-03-19 11:44:10 +0100

    Model configuration finished, just need to train on several videos

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 from extraction import extract
 from processing import process_mp, calculate_line_equation
+from prediction import *
+from keras.models import load_model
 import json
 
 
@@ -11,8 +15,10 @@ def calculate_graphs(data: list):
     for i in range(len(data)):
         for j in range(len(data[i])):
             if i == 0:
+                data[i][j].direct_graph = "firstFrame"
                 continue
             data[i][j].direct_graph = calculate_line_equation(data[i - 1][3], data[i][3], i)
+
 
 def save_to_json(path: str, data: list):
     # Initialize an empty dictionary to store the JSON data
@@ -25,7 +31,7 @@ def save_to_json(path: str, data: list):
 
         # Iterate over each FrameData object in the item
         for frame_data in item:
-            landmark = "Landmark"+str(frame_data.landmark+1)
+            landmark = "Landmark" + str(frame_data.landmark + 1)
             if landmark not in frame_data_by_landmark:
                 frame_data_by_landmark[landmark] = []
 
@@ -33,7 +39,6 @@ def save_to_json(path: str, data: list):
                 frame_data.relative[0],
                 frame_data.relative[1],
                 frame_data.relative[2],
-                frame_data.direct_graph
             ]
             frame_data_by_landmark[landmark].append(frame_data_list)
 
@@ -41,11 +46,11 @@ def save_to_json(path: str, data: list):
         json_data[f"frame{i}"] = frame_data_by_landmark
 
     # Write the dictionary to a JSON file
-    with open(path+"can1.json", "w") as json_file:
+    with open(path + "can1.json", "w") as json_file:
         json.dump(json_data, json_file, indent=4)
 
-# list [ [ 21 landmarks in here (FrameData) ], [ 21 landmarks in here (FrameData) ], [ 21 landmarks in here (FrameData) ] ]
-if __name__ == '__main__':
+
+def extract_and_save_data():
     video_path = '../data/videos/can1.MOV'
     data_path = './data/'
     extracted_frames = extract(video_path)
@@ -56,4 +61,48 @@ if __name__ == '__main__':
     calculate_graphs(all_data)
 
     save_to_json(data_path, all_data)
+
+
+def load_data_input_to_model(save: bool):
+    # data path to json file
+    data_path = './data/can1.json'
+    data: dict
+    file_name: str
+    data, file_name = load_json(data_path)
+    sequences: list = []
+    labels: list = [file_name, file_name]
+    result = fit_data_to_sequence(data)
+    sequences.append(result)
+    sequences.append(result)
+    define_and_train_model(sequences, labels, save)
+
+
+def load_and_use_model():
+    # Load the model
+    model = load_model('gesture_recognition_model.keras')
+    # Predict
+    data_path = './data/can1.json'
+    data: dict
+    file_name: str
+    data, file_name = load_json(data_path)
+    sequences: list = []
+    labels: list = [file_name, file_name]
+    result = fit_data_to_sequence(data)
+    sequences.append(result)
+    padded_sequences, labels, max_length = prepare_sequences(sequences, labels)
+    padded_sequences = np.array(padded_sequences)
+    prediction = model.predict(padded_sequences)
+    predicted_labels = np.argmax(prediction, axis=1)
+    class_labels = {0: "Can"}  # Update this dictionary with your class labels
+
+    # Map predicted class indices to their corresponding labels
+    predicted_labels = [class_labels[idx] for idx in predicted_labels]
+    print("Predicted Labels:", predicted_labels)
+
+
+# list [ [ 21 landmarks in here (FrameData) ], [ 21 landmarks in here (FrameData) ], [ 21 landmarks in here (FrameData) ] ]
+if __name__ == '__main__':
+    # extract_and_save_data()
+    #load_data_input_to_model(False)
+    load_and_use_model()
     # print("length of data: " + str(len(all_data)))
---
+++ app/prediction/label_predictor.py
@@ -0,0 +1,76 @@
+import json
+from typing import Tuple, Any
+
+import numpy as np
+from keras import Sequential
+from keras.src.layers import LSTM, Dense
+from keras.src.utils import pad_sequences
+from keras.models import save_model
+
+
+def load_json(path: str) -> tuple[dict, str]:
+    with open(path, "r") as json_file:
+        data = json.load(json_file)
+    # file_name without .json
+    file_name = path.split("/")[-1].split(".")[0]
+    print(f"Loaded data from {file_name}")
+    # remove all numbers from the text
+    file_name = ''.join([i for i in file_name if not i.isdigit()])
+    return data, file_name
+
+
+def fit_data_to_sequence(data: dict):
+    sequences: list = []
+    i = 0
+    for frame in data.values():
+        landmarks = []
+        for landmark_data in frame.values():
+            landmarks.append(list(landmark_data)[:3])  # Extract only the first 3 values
+        flattened_list = [item for sublist in landmarks for item in sublist]
+        sequences.append(flattened_list)
+    return sequences
+
+
+def prepare_sequences(all_sequences: list, all_sequence_labels: list) -> Tuple[list, list, int]:
+    # Combine sequences and labels
+    sequences = []
+    labels = []
+
+    for i, sequence in enumerate(all_sequences):
+        sequences.extend(sequence)
+        if str.lower(all_sequence_labels[i]) == "can":
+            labels.extend([[1, 0, 0]] * len(sequence))
+        else:
+            labels.extend([[0, 1, 0]] * len(sequence))
+
+    # Pad sequences to ensure equal length
+    max_length = max(len(seq) for seq in sequences)
+    padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post', dtype="float32")
+
+    return padded_sequences, labels, max_length
+
+
+def define_and_train_model(all_sequences: list, all_sequence_labels: list, save: bool = False):
+    # Prepare sequences and labels
+    padded_sequences, labels, max_length = prepare_sequences(all_sequences, all_sequence_labels)
+
+    # Convert to numpy arrays
+    padded_sequences = np.array(padded_sequences)
+    labels = np.array(labels)
+
+    # Define the model
+    model = Sequential([
+        LSTM(64, return_sequences=False, input_shape=(max_length, 3)),  # Assuming each data point has 4 features
+        Dense(3, activation='softmax')  # Assuming 3 output classes: "Fish", "Tank", "Nose"
+    ])
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    # Train the model
+    model.fit(padded_sequences, labels, epochs=10, batch_size=1, validation_split=0.2)
+
+    if not save:
+        save_model(model, "gesture_recognition_model.keras")
