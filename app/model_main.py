86cce018719a70b1229e25bc75c30dbbb8cdeee1
NickDue <n.duerlund@gmail.com>
2024-04-18 16:57:48 +0200

    Trained new models

--- app/model_main.py
+++ app/model_main.py
@@ -237,9 +237,9 @@ def load_and_use_new_model():
 if __name__ == '__main__':
     print("nice")
     #extract_and_save_data(subpath="coordinates")
-    extract_and_save_data(subpath="graphs", v2=True)
+    #extract_and_save_data(subpath="graphs", v2=True)
     #load_data_input_to_model(True, 'train/coordinates')
-    #load_data_input_to_model_v2(True, 'train/graphs')
+    load_data_input_to_model_v2(True, 'train/graphs')
     # load_and_use_model()
     #load_and_use_new_model()
     # load_single_video_and_predict()
