b9146adb69866baf0e55dba51a7541a19ceab215
NickDue <n.duerlund@gmail.com>
2024-05-15 00:54:02 +0200

    locustfile changes for testing

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -1,16 +1,21 @@
 import time
 from locust import HttpUser, task, between
-
+import random
 
 class StatusUser(HttpUser):
-    wait_time = between(1, 2)
+    wait_time = between(1, 4)
+
+    #@task
+    #def predict_with_model(self):
+     #   with open('../app/data/videos/valid/thumb1.MOV', 'rb') as f:
+     #       self.client.post("/model/predict", files={"video_file": f})
 
     @task
-    def get_status(self):
-        self.client.get("/status/")
-
-    @task
-    def predict_with_model(self):
-        with open('../app/data/videos/valid/thumb1.MOV', 'rb') as f:
-            self.client.post("/model/predict", files={"video_file": f})
+    def load_test_with_random_videos(self):
+        model = "coordinates"
+        labels = ['thumb', 'peace', 'can']
+        number_to_100 = random.randint(1, 100)
+        random_label = random.choice(labels)
+        with open(f'../app/data/videos/valid/{random_label}{number_to_100}.MOV', 'rb') as f:
+            self.client.post(f"/model/predict_{model}", files={"video_file": f})
 
